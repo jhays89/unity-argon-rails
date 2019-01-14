61085a50577914b48ff3610738f16f81006abc42
jhays89 <justin@tridigitalmarketing.com>
2019-01-13 20:59:03 -0600

    initial push should have ignore library folder

---
+++ Assets/SplashController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashController : MonoBehaviour {
+
+    // Use this for initialization
+    private void Awake()
+    {
+        var musicPlayer = GameObject.Find("Music Player");
+        DontDestroyOnLoad(musicPlayer);
+    }
+
+    void Start () {
+        Invoke("LoadFirstLevel", 3f);
+	}
+	
+	// Update is called once per frame
+	void LoadFirstLevel () {
+        SceneManager.LoadScene(1);
+	}
+}

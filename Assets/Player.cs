1ebcc08ec3579bcab9ae25235988273b6f4e93f8
Justin Hays <justin@tridigitalmarketing.com>
2019-01-15 13:45:33 -0600

    initiated player logic

---
+++ Assets/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // This is used instead of 'key' -- allows for cross platform and I assume is good practice. can be found in edit > Project settings > input
+    }
+}
